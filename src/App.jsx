b11ca44efdf149b0d5353648bdfb4d9711d59403
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-03-09 18:11:00 +0100

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import './App.css';
 import React, {useEffect} from 'react';
 import {useContentful} from 'react-contentful'
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, NavLink} from 'react-router-dom';
 import {client} from './client.js';
+import Hero from './Hero';
+import Author from './components/Author';
+import Blogpost from './components/Blogpost';
+import Partner from './components/Partner';
+import Recipe from './components/Recipe';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+
 
 function App() {
 
 
-useEffect(() => {
-
-  client.getEntries({content_type:'recipe'}).then(response => console.log(response)
-       
-    );
-  }, [])
-
-
-
-
   return (
+    <>
     <div className="App">
       <header className="App-header">
+        <img className="logo"/>
+        <nav>
 
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/recipes">Recipes</NavLink>
+            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/authors">Authors</NavLink>
+            <NavLink to="/partners">Partners</NavLink>
+
+        </nav>
+        <Hero/>
       </header>
       <main>
 
       </main>
       <footer>
-
+        
       </footer>
     </div>
+    </>
   );
 }
 
