bcf352b75e3496020509f852dbac30e3aff40252
Vita <lacroix@rambler.ru>
2022-03-10 16:58:28 +0100

    blogposts and card added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import React, {useEffect} from 'react';
 import {useContentful} from 'react-contentful'
 import {Routes, Route} from 'react-router-dom';
 import {client} from './client.js';
+import Blogpost from "./components/Blogpost";
+import Card from "react-bootstrap/Card";
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
 
@@ -14,16 +18,26 @@ useEffect(() => {
     );
   }, [])
 
-
-
-
   return (
     <div className="App">
       <header className="App-header">
 
       </header>
       <main>
-
+        <>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+</>
+      <Blogpost />
       </main>
       <footer>
 
