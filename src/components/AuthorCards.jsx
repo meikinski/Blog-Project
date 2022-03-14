b470c36b2a242afe98bf9c4a2d391f42b38db3b2
Lina Ho <yenthi.ho.lina@gmail.com>
2022-03-14 17:54:34 +0100

    added styling

--- src/components/AuthorCards.jsx
+++ src/components/AuthorCards.jsx
@@ -7,33 +7,32 @@ export default function AuthorCards({
   socialMedia,
   title,
 }) {
-  const [showText, setShowText] = useState(false);
-
-  const handleClick = () => {
-    if (showText === false) {
-      setShowText(true);
-    } else {
-      setShowText(false);
-    }
-  };
+  // const [showText, setShowText] = useState(false);
+  const [showInsta, setShowInsta] = useState(false);
+  // const handleClick = () => {
+  //   if (showText === false) {
+  //     setShowText(true);
+  //   } else {
+  //     setShowText(false);
+  //   }
+  // };
   return (
     <div className="authors">
       <div className="authorImgContainer">
-        <img className="authorImg" src={picture} alt="Author" />
+        <a href={socialMedia} className="socialMedia">
+          <img className="authorImg" src={picture} alt="Author" />
+        </a>
+        <div className="overlay">
+          <a href={socialMedia} className="socialMedia">
+            <div class="text">Follow on Instagram</div>
+          </a>
+        </div>
       </div>
       <h3 className="authorTitle">{title}</h3>
       <div className="authorBtn-container">
-        <button className="authorBtn" onClick={handleClick}>
-          About {name}
-        </button>
+        <div className="authorBtn">About {name}</div>
       </div>
-      {showText && <div className="shortIntro">{shortIntro}</div>}
-      <br />
-      {showText && (
-        <a href={socialMedia} className="socialMedia">
-          Follow me on Instagram
-        </a>
-      )}
+      <div className="shortIntro">{shortIntro}</div>
     </div>
   );
 }
